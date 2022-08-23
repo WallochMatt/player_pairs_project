f05278f5b12ce94136ad0a7ddd4eb09efe88fa94
WallochMatt <matthewrwalloch@gmail.com>
2022-08-23 17:15:11 -0500

    feat: Added new functions that better suit the needs of the project

--- main.py
+++ main.py
@@ -15,51 +15,84 @@
 #function that deals hands? tor multiple players?
 
 import random
-
+#Note: I could make a while loop in the run game function to start the next game, keep track of score and end game..
 def run_game():
     player_count = counts_players()
-    deck = makes_deck()
-    player_hands = deals(deck) #it needs access to the players and deck from those respective functions
-    checks_results(player_count, player_hands)
+    full_deck = makes_deck()
+    hand = deals(full_deck) #this ran an additional instence
+    test = assign_hands(player_count, full_deck) #may need to uncap, but it works so far
+    pairs = copy_list(hand) #made a dupe
+    #result = checks_results(hand)
+    winner(player_count, pairs)
 
-#assigns players
 def counts_players():
     amount_of_players = 2#int(input("How many Players? (10 Player limit)")) #going to make a list of players, and I might be able to create a dictionary that way too
     players = []
-    for player in range(amount_of_players): #i changed this from index to player, just in case it breaks
+    for player in range(amount_of_players):
         players.append(player)
     return players
 
-# #creates full deck
 def makes_deck():
     card_types = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
     deck = []
     for card in card_types * 4:                
-        deck.append(card) #now i cn shuffle with random
+        deck.append(card)
     return deck
 
-#deals and "shuffles", #the display portion of this will have to be its own function
 def deals(deck):
     player_hand = []
     for index in range(5):#?
         card_in_hand = random.choice(deck)
         deck.remove(card_in_hand)
         player_hand.append(card_in_hand)
-    return player_hand #may need to move out a scope
-       
-def hud(players, card_in_hand):
-    for player in players:#range(amount_of_players):
-        print(f" Player {str(player + 1)}:")
-        print(card_in_hand)
-        #print(f"{deck} remains") shows card loss progression
+    return player_hand
 
-#check for winner/results
-# it is checking all hands, rather than just a single player
 
-def checks_results(players, player_hand):
+def assign_hands(player_count, full_deck):
+    for player in player_count:
+        print(f" Player {str(player + 1)}:") #"HUD" function made obsolete
+        hand = deals(full_deck)
+        print(hand)
+        copy_list(hand)
+        #checks_results(hand)
+    return hand #needed?
+
+
+def copy_list(hand):
+    copies = {0} #use length of the set - 1
+    for index in hand:
+        if hand.count(index) > 1:
+            copies.add(index)
+    copies.discard(0)
+    #print(f" {len(copies)} matches")
+    print(len(copies))
+    return copies
+
+
+            
+def winner(players, copies):
     for player in players:
-        for index in player_hand:
-            if player_hand.count(index) > 1:
-                print("M")
+        if len(copies) == 0:
+            print("No pairs")
+        if player != player[-1]:
+            if len(copies) > len(copies[player + 1]): #find away around this out of range
+                print(f" Player {str(player + 1)} wins")
+            elif len(copies) == len(copies[player + 1]):
+                print("Tie")
+
+
+
+
+
+
 
 run_game()
+
+# def copy_list(hand): #alt for check results
+#     copies = [] # couldn't this be a set?
+#     for index in hand:
+#         if hand.count(index) > 1:
+#             copies.append(index)
+#             hand.remove(index)
+#             #print(f" {len(copies)} matches")
+#             #return copies
