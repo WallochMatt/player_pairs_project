cc7b7ec36d97582ddac95dc7a1c99148e7725a0f
WallochMatt <matthewrwalloch@gmail.com>
2022-08-24 01:49:05 -0500

    feat: added the  ability to choose rounds and number of players. As well as finished the remaining functions.

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-# for player in players:#works on scratch pad before functions were made
-#     open_hand = set()
-#     if [card for card in player_hand if card in open_hand or open_hand.add(card)]:
-#         print("MATCH")
-#^this is list comprehension, I can't mess with this yet
-
-#pure functions only, everything must be inside a function, do not use global at all
-
-#function: stores 52 cards in a list, try to for loop 4 times
-
-#shuffle function? would random py function suffice? 
-
-# funtion to detect paris in player hands and determines winner  
-
-#function that deals hands? tor multiple players?
 
 import random
-#Note: I could make a while loop in the run game function to start the next game, keep track of score and end game..
+
 def run_game():
+    round= 0 
+    number_of_rounds = select_rounds()
     player_count = counts_players()
-    full_deck = makes_deck()
-    hand = deals(full_deck) #this ran an additional instence
-    test = assign_hands(player_count, full_deck) #may need to uncap, but it works so far
-    pairs = copy_list(hand) #made a dupe
-    #result = checks_results(hand)
-    winner(player_count, pairs)
+    while round < number_of_rounds:
+
+        full_deck = makes_deck()
+    
+        updated_player_list = assign_hands(player_count, full_deck) #may need to uncap, but it works so far
+        
+        declare_winner(updated_player_list)
+        round += 1
+        input("Press enter for next round")
 
 def counts_players():
-    amount_of_players = 2#int(input("How many Players? (10 Player limit)")) #going to make a list of players, and I might be able to create a dictionary that way too
+    #amount_of_players = 4
+    amount_of_players = int(input("How many Players? (10 Player limit)")) #going to make a list of players, and I might be able to create a dictionary that way too
     players = []
     for player in range(amount_of_players):
         players.append(player)
     return players
+    # amount_of_players = 2#int(input("How many Players? (10 Player limit)")) #going to make a list of players, and I might be able to create a dictionary that way too
+    # players = [] * amount_of_players#empty spaces?
+    # return players
+
 
 def makes_deck():
     card_types = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
@@ -49,50 +44,62 @@ def deals(deck):
 
 
 def assign_hands(player_count, full_deck):
-    for player in player_count:
-        print(f" Player {str(player + 1)}:") #"HUD" function made obsolete
+    for player_index in range(len(player_count)):
+        print(f" {get_player_id(player_index)}")
         hand = deals(full_deck)
         print(hand)
-        copy_list(hand)
-        #checks_results(hand)
-    return hand #needed?
+        list_of_pairs = find_pairs(hand)
+        num_of_pairs = len(list_of_pairs)
+        player_count[player_index] = num_of_pairs
+      
+    return player_count
 
 
-def copy_list(hand):
-    copies = {0} #use length of the set - 1
-    for index in hand:
-        if hand.count(index) > 1:
-            copies.add(index)
-    copies.discard(0)
-    #print(f" {len(copies)} matches")
-    print(len(copies))
+def find_pairs(hand):
+    copies = set()
+    for card_index in hand:
+        if hand.count(card_index) > 1:
+            copies.add(card_index)
+   
+    print(f"{len(copies)} pairs!")
     return copies
 
-
             
-def winner(players, copies):
-    for player in players:
-        if len(copies) == 0:
-            print("No pairs")
-        if player != player[-1]:
-            if len(copies) > len(copies[player + 1]): #find away around this out of range
-                print(f" Player {str(player + 1)} wins")
-            elif len(copies) == len(copies[player + 1]):
-                print("Tie")
+def declare_winner(player_count):
+    #I have chosen to leave this in a comment mess for the sake of feedback 
+    largest_pairing = 0
+    tie_call = 0
+    for current_player_index in range(len(player_count)):
+        if (player_count[current_player_index] == player_count[largest_pairing]):# and (player_count[current_player_index] != player_count[largest_pairing]):
+             print(f"{get_player_id(largest_pairing)} tied!!")
+             tie_call += 1
+        
+
+        # if(player_count[current_player_index] > player_count[largest_pairing]):
+        #     largest_pairing = current_player_index
+
+        # for current_player_index in range(len(player_count)):
+        #     if current_player_index != (len(player_count) - 1):
+
+        #         if current_player_index == player_count[current_player_index + 1]:
+        #             print(f"TIE with {get_player_id(current_player_index)}")
+        #             break
+        #         else:
+        #             pass
+        if(player_count[current_player_index] > player_count[largest_pairing]) and tie_call == 0:
+            largest_pairing = current_player_index
+
+    print(f"{get_player_id(largest_pairing)} won with {player_count[largest_pairing]} pairs!!!")
 
 
+def get_player_id(index):
+    return f"\n Player {str(index + 1)}:"
 
-
+def select_rounds():
+    number_of_rounds = int(input("Select number of rounds: "))
+    return number_of_rounds
 
 
 
 run_game()
 
-# def copy_list(hand): #alt for check results
-#     copies = [] # couldn't this be a set?
-#     for index in hand:
-#         if hand.count(index) > 1:
-#             copies.append(index)
-#             hand.remove(index)
-#             #print(f" {len(copies)} matches")
-#             #return copies
